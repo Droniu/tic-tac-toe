2a8e04071d150f7d36731812233442fbb5cab5f8
Droniu <dron.official@yahoo.com>
2021-04-19 08:06:32 +0200

    helloworld

--- simple/main.s
+++ simple/main.s
@@ -24,19 +24,106 @@
         inx ; ff + 1 = 00
 
         ; Clean PPU registers
-        stx #$2000
-        stx #$2001
+        stx $2000
+        stx $2001
+
+        stx $4010 ; Disable PCM channel
+        
+        : ; anonymous label
+        bit $2002; $2002 is telling if PPU is currently drawing
+        bpl :- ; branch if not in VBLANK (not waiting for another screen)
+        ; :- means go to last anonymous label, :+ go to next anonymous label
+
+        txa 
+
+    ; Memory clearing
+    CLEARMEM:
+        sta $0000, X ; $0000 -> $00FF
+        sta $0100, X
+
+        sta $0300, X
+        sta $0400, X
+        sta $0500, X 
+        sta $0600, X
+        sta $0700, X
+
+        ; one of the ranges is for sprie data. we can choose any
+        lda #$FF
+        sta $0200, X
+        lda #$00
+
+        inx
+        bne CLEARMEM 
+
+    ; wait for VBLANK again
+    :
+        bit $2002
+        bpl :-
+
+        lda #$02 ; MSB of sprite data memory range
+        sta $4014 ; Object Attribute Memory DMA register
+        nop ; ppu needs a moment to load data
+
+        ; preparing ppu memory 
+        lda #$3F
+        sta $2006
+        lda #$00
+        sta $2006
+
+        ldx #$00
+
+    LoadPalettes:
+        lda PaletteData, X
+        sta $2007 ; $3F00, $3F01, and so on
+
+        ; loop
+        inx
+        cpx #$20 ; 32 in decimal
+        bne LoadPalettes
+
+        ldx #$00
     
+    LoadSprites:
+        lda SpriteData, X
+        sta $0200, X
+        inx
+        cpx #$20
+        bne LoadSprites
+    
+    ; enable interrupts
+        cli
+        lda #%10010000 ; enable NMI, use second set of sprites
+        sta $2000
+        ; enable drawing in leftmost 8px of screen and in general
+        lda #%00011110
+        sta $2001
+
     Forever:
         jmp Forever ; prevents going to NMI after pushing reset
 
     NMI:
+        ; loading sprites from $0200 to ppu memory
+        lda #$02 ; (2 because this is MSB)
+        sta $4014
+        
         rti ; interrupt return
 
+    PaletteData: ;example
+        .byte $22,$29,$1A,$0F,$22,$36,$17,$0f,$22,$30,$21,$0f,$22,$27,$17,$0F  ;background palette data
+        .byte $22,$16,$27,$18,$22,$1A,$30,$27,$22,$16,$30,$27,$22,$0F,$36,$17  ;sprite palette data
+    SpriteData: ;example
+        .byte $08, $00, $00, $08
+        .byte $08, $01, $00, $10
+        .byte $10, $02, $00, $08
+        .byte $10, $03, $00, $10
+        .byte $18, $04, $00, $08
+        .byte $18, $05, $00, $10
+        .byte $20, $06, $00, $08
+        .byte $20, $07, $00, $10
 
   
 .segment "VECTORS" ; special address which 6502 needs
     .word NMI ; refresh time
     .word Reset ; reset button
 .segment "CHARS" ; graphical data
-    .incbin "graphics.chr"
+    .incbin "example_sprites.chr"
